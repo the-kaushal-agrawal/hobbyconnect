2119e010dc523ddd279668b8c81d21f1d689cbf5
Kaushal Agrawal <kaushalagr.02@gmail.com>
2023-07-15 20:58:02 +0530

    changes done

--- dbs/connect.js
+++ dbs/connect.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 
 const dotenv = require('dotenv');
-const URL ="mongodb+srv://kaushalacm20:V9jFprRkVYa9YjfB@cluster1.ypjvrlw.mongodb.net/?retryWrites=true&w=majority
+const URL ="mongodb+srv://kaushalacm20:V9jFprRkVYa9YjfB@cluster1.ypjvrlw.mongodb.net/?retryWrites=true&w=majority"
 
 
 //let URL = process.env.DATABASEURL || 'mongodb://localhost:27017/hobby';
