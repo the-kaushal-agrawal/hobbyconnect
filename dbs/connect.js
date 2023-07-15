958a1f01075fb4228fb329f2c227931151939e37
Kaushal Agrawal <kaushalagr.02@gmail.com>
2023-07-15 20:51:07 +0530

    Uploading to Git

---
+++ dbs/connect.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+
+const dotenv = require('dotenv');
+const URL ="mongodb+srv://kaushalacm20:V9jFprRkVYa9YjfB@cluster1.ypjvrlw.mongodb.net/?retryWrites=true&w=majority
+
+
+//let URL = process.env.DATABASEURL || 'mongodb://localhost:27017/hobby';
+
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}).then(() => {
+    console.log(`connected successfully`);
+}).catch(() => {
+    console.log('no connection');
+});
